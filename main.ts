c05c78699d70edfb327b4cdbb10f4888fdd28c3a
sclim <kiate.lim@gmail.com>
2022-08-18 16:13:39 +0800

    Made ultrasonic reading a background process that runs at every 200ms interval.

--- main.ts
+++ main.ts
@@ -28,10 +28,8 @@ if (board_ver == "1") {
     const_2divspeed = 39
 }
 
-let elapsed_time = 0
-let prev_time = 0
 
-    
+
 // Headlight channel.
 enum HeadlightChannel {
     //% block="left"
@@ -95,13 +93,56 @@ enum LinePosition {
 //% groups=['Headlights', 'DC Motors', 'Maker Line', 'Ultrasonic']
 namespace zoombit {
 
+    // Ultrasonic sensor distance.
+    let usDistance = 255
+
+
+    // Background function to read ultrasonic sensor distance at 200ms interval.
+    control.inBackground(function() {
+        while(1) {
+            // Transmit a pulse.
+            pins.digitalWritePin(US_TRIG_PIN, 0)
+            control.waitMicros(2)
+            pins.digitalWritePin(US_TRIG_PIN, 1)
+            control.waitMicros(10)
+            pins.digitalWritePin(US_TRIG_PIN, 0)
+
+            // Read the echo.
+            const pulse = pins.pulseIn(US_ECHO_PIN, PulseValue.High, 255 * const_2divspeed)
+
+            // No echo detected.
+            if (pulse == 0) {
+                usDistance = 255
+            }
+            else {
+                usDistance = Math.idiv(pulse, const_2divspeed)
+            }
+            basic.pause(200);   // Recommended minimum time between readings 
+                                // for ultrasonic model RCWL-9610 is 200ms.
+        }
+    })
+
+
+    /**
+     * Return distance measured by ultrasonic sensor in centimeters (cm).
+     * Distance = 1cm - 255cm. Return '255' if distance is > 255cm or no echo is detected.
+     */
+    //% group="Ultrasonic"
+    //% weight=14
+    //% blockGap=8
+    //% blockId=zoombit_read_ultrasonic
+    //% block="ultrasonic distance (cm)"
+    export function readUltrasonic(): number {
+        return usDistance
+    }
+
+
 
     // State for headlights.
     let headlightsState = {
         left: 0,
         right: 0
     };
-
 
 
     /**
@@ -336,53 +377,4 @@ namespace zoombit {
         return position;
     }
 
-
-
-    /**
-     * Return distance measured by ultrasonic sensor in centimeters (cm).
-     * Distance = 1cm - 255cm. Return '255' if distance is > 255cm or no echo is detected.
-     */
-    //% group="Ultrasonic"
-    //% weight=14
-    //% blockGap=8
-    //% blockId=zoombit_read_ultrasonic
-    //% block="ultrasonic distance (cm)"
-    export function readUltrasonic(): number {
-        
-        do {
-            elapsed_time = control.millis() - prev_time
-            if (elapsed_time < 0) {
-                // Prevent -ve value when timer overflows.
-                prev_time = control.millis()
-                break
-            }
-            else if(elapsed_time < 200) {
-                // Recommended minimum time between readings 
-                // for ultrasonic model RCWL-9610 is 200ms.
-                basic.pause(elapsed_time)
-            }
-
-            // Transmit a pulse.
-            pins.digitalWritePin(US_TRIG_PIN, 0)
-            control.waitMicros(2)
-            pins.digitalWritePin(US_TRIG_PIN, 1)
-            control.waitMicros(10)
-            pins.digitalWritePin(US_TRIG_PIN, 0)
-
-            // Read the echo.
-            const pulse = pins.pulseIn(US_ECHO_PIN, PulseValue.High, 255 * const_2divspeed)
-
-            // No echo detected.
-            if (pulse == 0) return 255
-
-            prev_time = control.millis()
-            
-            // Tuned for microbit to get the right value in cm.
-            return Math.idiv(pulse, const_2divspeed)
-
-        // Repeat if -ve value detected.
-        } while (elapsed_time < 0)
-
-        return 255
-    }
 }
